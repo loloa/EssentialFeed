6a7ca1baa7e16be8d1d5f63e942fa0e1863cdf43
loloa <lilia707@gmail.com>
2023-08-12 17:59:45 +0300

    Created Feed Item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by אליסה לשין on 12/08/2023.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
