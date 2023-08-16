d52065c4e9cee1dcb3eddd6a96ae68f15d1a9491
loloa <lilia707@gmail.com>
2023-08-16 15:40:37 +0300

    Removed redundant test and code

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -12,7 +12,6 @@ class FeedStore {
     
     typealias DeletionCompletion = ((Error?) -> Void)
     var deleteCachedFeedCallCount: Int = 0
-    var insertCallCount: Int = 0
     var insertions = [(items: [FeedItem], timestamp: Date)]()
     
     private var deletionCompletions = [DeletionCompletion]()
@@ -28,7 +27,7 @@ class FeedStore {
         deletionCompletions[index](nil)
     }
     func insert(_ items: [FeedItem], timestamp: Date) {
-        insertCallCount += 1
+        
         insertions.append((items, timestamp))
     }
 }
@@ -78,18 +77,9 @@ final class CacheFeedUseCaseTests: XCTestCase {
         
         sut.save(items)
         store.completeDeletion(with: deletionError)
-        XCTAssertEqual(store.insertCallCount, 0)
+        XCTAssertEqual(store.insertions.count, 0)
     }
-    
-    func test_save_requestsNewCacheInsertionOnSuccessfulDeletion() {
-     
-        let items = [uniqueItem(),uniqueItem()]
-        let (sut, store) = makeSUT()
-        
-        sut.save(items)
-        store.completeDeletionSuccessfully()
-        XCTAssertEqual(store.insertCallCount, 1)
-    }
+ 
  
     func test_save_requestsNewCacheInsertionWithTimeStampOnSuccessfulDeletion() {
      
