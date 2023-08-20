7c22d696a739dc05e9a9bb026428c4d1f3890f43
loloa <lilia707@gmail.com>
2023-08-20 11:07:50 +0300

    Extract FeedCacheSpy into a shared scope to remove duplication

---
+++ EssentialFeedTests/Feed Cache/FeedStoreSpy.swift
@@ -0,0 +1,49 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by אליסה לשין on 20/08/2023.
+//
+
+import Foundation
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+    
+    /*
+     we have order dependency on store insert can be done only after deletion
+     so we need garantee not only that the methods were called but also called in write order
+     we ned to merge all info in one stuff to use it in assertions
+     */
+    
+    enum ReceivedMessages: Equatable {
+        case deleteCachedFeed
+        case insert([LocalFeedImage], Date)
+    }
+    private (set) var receivedMessages = [ReceivedMessages]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion){
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    func completeDeletionSuccessfully(at index: Int = 0){
+        deletionCompletions[index](nil)
+    }
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+ }
