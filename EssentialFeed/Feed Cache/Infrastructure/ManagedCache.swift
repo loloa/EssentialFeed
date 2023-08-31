963a757766b1a43102430355915f09b288511dbe
loloa <lilia707@gmail.com>
2023-08-31 17:01:33 +0300

    Separate CoreData managed model classes data from helpers with extensions

--- EssentialFeed/Feed Cache/Infrastructure/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedCache.swift
@@ -16,15 +16,19 @@ internal class ManagedCache: NSManagedObject {
     var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
-    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+ 
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+}
+
+extension ManagedCache {
+    
+    internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         
         let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
-    }
-    
-    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-        try find(in: context).map(context.delete)
-        return ManagedCache(context: context)
     }
 }
--- EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
@@ -14,19 +14,23 @@ internal class ManagedFeedImage: NSManagedObject {
    @NSManaged var location: String?
    @NSManaged var url: URL
    @NSManaged var cache: ManagedCache
-   
-   static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-       
-       return NSOrderedSet(array: feed.map { local in
-           let managed = ManagedFeedImage(context: context)
-           managed.id = local.id
-           managed.imageDescription = local.description
-           managed.location = local.location
-           managed.url = local.url
-           return managed
-       })
-   }
+ 
    var local: LocalFeedImage {
        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
    }
+}
+
+extension ManagedFeedImage {
+    
+    internal static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        
+        return NSOrderedSet(array: feed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
 }
