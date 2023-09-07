c4efca8271988cc8cb5a915f8198cb769a7d6dd2
loloa <lilia707@gmail.com>
2023-09-07 11:33:04 +0300

    `FeedViewController` loads view on view did load

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,11 +6,19 @@
 //
 
 import XCTest
+import UIKit
 
-final class FeedViewController {
+final class FeedViewController: UIViewController {
     
-    init(loader: FeedViewControllerTests.LoaderSpy) {
-         
+    private var loader: FeedViewControllerTests.LoaderSpy?
+    
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader?.load()
     }
 }
 
@@ -23,8 +31,21 @@ final class FeedViewControllerTests: XCTestCase {
         
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    // MARK: - Helpers
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
         
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
