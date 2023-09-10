4b921e801b2e2e9bbbfbdc61ba508bf7faeee70b
loloa <lilia707@gmail.com>
2023-09-10 21:15:49 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by אליסה לשין on 10/09/2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+
+}
