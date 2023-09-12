d6dcb0f2002dd5a8785b229914e5e11050d4078d
loloa <lilia707@gmail.com>
2023-09-12 12:43:59 +0300

    Extract duplicate logic into a reusable UIControl extension

---
+++ EssentialFeediOSTests/UIControl+TestHelper.swift
@@ -0,0 +1,19 @@
+//
+//  UIControl+TestHelper.swift
+//  EssentialFeediOSTests
+//
+//  Created by אליסה לשין on 12/09/2023.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
+
