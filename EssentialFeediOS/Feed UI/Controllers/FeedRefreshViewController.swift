4cd16b8f793d76e9977ed53ee4e04ef0742ea97f
loloa <lilia707@gmail.com>
2023-09-13 17:05:39 +0300

    fixed CI

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+ 
 
 final class FeedRefreshViewController: NSObject {
+    
+    //private let disposeBag = DisposeBag()
     
     private let viewModel: FeedViewModel
  
@@ -34,4 +37,18 @@ final class FeedRefreshViewController: NSObject {
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return view
     }
+    
+    /*
+     private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
+         view.rx.controlEvent(.valueChanged)
+                 .bind(to: viewModel.loadFeed)
+                 .disposed(by: disposeBag)
+
+         viewModel.isLoading
+             .bind(to: view.rx.isRefreshing)
+             .dispose(by: disposeBag)
+
+         return view
+     }
+     */
 }
