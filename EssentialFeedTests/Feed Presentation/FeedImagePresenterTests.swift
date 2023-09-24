853cd8f1fcfa454820df91bd41a52ac445dba0a0
loloa <lilia707@gmail.com>
2023-09-24 13:41:35 +0300

    Extract creation of `FeedImageLoader` to makeSUT method

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -18,12 +18,25 @@ final class FeedImagePresenterTests: XCTestCase {
 
     func test_init_doesNotSendMessageToView(){
         
-        let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view)
+        let (_, view) = makeSUT()
+        
         XCTAssertTrue(view.messages.isEmpty, "Expected no messages")
     }
 
+ 
+    //MARK: -
+    
     private  class ViewSpy {
         let messages = [Any]()
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter,view: ViewSpy) {
+        
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return (sut, view)
     }
 }
