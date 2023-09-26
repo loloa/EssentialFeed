fbe2f1558767d1b04a6fb0573ce867f67ee76fdc
loloa <lilia707@gmail.com>
2023-09-26 19:45:37 +0300

    Add test

---
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteFeedImageDataLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by אליסה לשין on 26/09/2023.
+//
+
+import XCTest
+
+final class RemoteFeedImageDataLoaderTests: XCTestCase {
+
+    
+
+}
