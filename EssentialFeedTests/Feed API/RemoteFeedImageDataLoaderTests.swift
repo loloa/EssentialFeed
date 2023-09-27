603eff37cdfe21173f9e326676d4ce2c4baac021
loloa <lilia707@gmail.com>
2023-09-27 09:53:54 +0300

    Creation of set extracted to factory method to prevent future possible breaking on changes

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -18,8 +18,8 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
 
     func test_init_doesNotPerformAnyURLRequest() {
         
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedImageDataLoader(client: client)
+        let (_, client) = makeSUT()
+        
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
@@ -28,6 +28,15 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     private class HTTPClientSpy {
         
          var requestedURLs = [URL]()
+    }
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, client)
+        
     }
 
 }
