777ff04a90cbddf7eb6d3ab0ce6237925f5eedce
loloa <lilia707@gmail.com>
2023-10-01 11:28:56 +0300

    extract creation of set into factory method, extract expect to help method

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -20,24 +20,42 @@ extension CoreDataFeedStore: FeedImageDataStore {
 final class CoreDataFeedImageDataStoreTests: XCTestCase {
 
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
+       
+        let sut = makeSUT()
         
+        expect(sut, completeWith: notFound(), for: anyURL())
+         
+    }
+    //MARK: -
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedImageDataStore {
         
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = URL(fileURLWithPath: "/dev/null")
         let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    private func notFound() -> FeedImageDataStore.Result {
+        
+        return .success(.none)
+    }
+    private func expect(_ sut: FeedImageDataStore,
+                        completeWith expectedResult: FeedImageDataStore.Result,
+                        for url: URL,
+                        file: StaticString = #file, line: UInt = #line) {
         
         let exp = expectation(description: "Waiting for load completion")
-        let expectedResult = FeedImageDataStore.Result.success(.none)
-        
-        sut.retrieve(dataForURL: anyURL()) { receivedResult in
+ 
+        sut.retrieve(dataForURL: url) { receivedResult in
              
             switch (receivedResult, expectedResult) {
                 
             case let (.success(receivedData), .success(expectedData)):
-                XCTAssertEqual(receivedData, expectedData)
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
                 
             default:
-                XCTFail("Expected \(expectedResult), got instead \(receivedResult)")
+                XCTFail("Expected \(expectedResult), got instead \(receivedResult)", file: file, line: line)
             }
             exp.fulfill()
         }
