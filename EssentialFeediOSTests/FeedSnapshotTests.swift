104d0f4e501f1cb7e806b82831e9a644df2ee071
loloa <lilia707@gmail.com>
2023-10-10 22:23:01 +0300

    Assert snapshots match the stored snapshots

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -16,7 +16,7 @@ class FeedSnapShotTests: XCTestCase {
         let sut = makeSUT()
         sut.display(emptyFeed())
         let snapshot = sut.snapshot()
-        record(snapshot: snapshot, named: "EMPTY_FEED")
+        assert(snapshot: snapshot, named: "EMPTY_FEED")
     }
     
     func test_feedWithContent() {
@@ -24,7 +24,7 @@ class FeedSnapShotTests: XCTestCase {
         let sut = makeSUT()
         sut.display(feedWithContent())
         let snapshot = sut.snapshot()
-        record(snapshot: snapshot, named: "FEED_WITH_CONTENT")
+        assert(snapshot: snapshot, named: "FEED_WITH_CONTENT")
     }
     
     func test_feedWithErrorMessage() {
@@ -32,16 +32,16 @@ class FeedSnapShotTests: XCTestCase {
         let sut = makeSUT()
         sut.display(.error(message: "An error message\n multiline error \n messge"))
         let snapshot = sut.snapshot()
-        record(snapshot: snapshot, named: "FEED_WITH_ERROR_MESSAGE")
+        assert(snapshot: snapshot, named: "FEED_WITH_ERROR_MESSAGE")
     }
     
     func test_feedWithFailedImageLoading() {
-            let sut = makeSUT()
-
-            sut.display(feedWithFailedImageLoading())
-
-            record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
-        }
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        assert(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
     
     //MARK: -
     
@@ -60,58 +60,96 @@ class FeedSnapShotTests: XCTestCase {
     private func feedWithContent() -> [ImageStub] {
         
         return [
-                    ImageStub(
-                        description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-                        location: "East Side Gallery\nMemorial in Berlin, Germany",
-                        image: UIImage.make(withColor: .red)
-                    ),
-                    ImageStub(
-                        description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-                        location: "Garth Pier",
-                        image: UIImage.make(withColor: .green)
-                    )
-                ]
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
     }
     
     private func feedWithFailedImageLoading() -> [ImageStub] {
-            return [
-                ImageStub(
-                    description: nil,
-                    location: "Cannon Street, London",
-                    image: nil
-                ),
-                ImageStub(
-                    description: nil,
-                    location: "Brighton Seafront",
-                    image: nil
-                )
-            ]
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
+            )
+        ]
+    }
+    
+    
+    
+    private func assert(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
+            let snapshotURL = makeSnapshotURL(named: name, file: file)
+            let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
+
+            guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
+                XCTFail("Failed to load stored snapshot at URL: \(snapshotURL). Use the `record` method to store a snapshot before asserting.", file: file, line: line)
+                
+                record(snapshot: snapshot, named: name)
+                assert(snapshot: snapshot, named: name)
+                return
+            }
+            
+            if snapshotData != storedSnapshotData {
+                let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+                    .appendingPathComponent(snapshotURL.lastPathComponent)
+                
+                try? snapshotData?.write(to: temporarySnapshotURL)
+                
+                XCTFail("New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), Stored snapshot URL: \(snapshotURL)", file: file, line: line)
+            }
         }
     
-    private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
-       
-        guard let snapshotData = snapshot.pngData() else {
-            XCTFail("Failed to generate PNG data representation from snapshot")
-            return
-        }
+    private func makeSnapshotURL(named name: String, file: StaticString) -> URL {
+        
         
         // Users/alisala/Desktop/PracticeApps/EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
         // Users/alisala/Desktop/PracticeApps/EssentialFeed/EssentialFeediOSTests/snapshots
         // Users/alisala/Desktop/PracticeApps/EssentialFeed/EssentialFeediOSTests/snapshots/EMPTY_FEED.png
-        let snapshotURL = URL(fileURLWithPath: String(describing: file))
-            .deletingLastPathComponent().appending(component: "snapshots")
-            .appending(component: "\(name).png")
-        
-        do {
-            try FileManager.default
-                .createDirectory(at: snapshotURL.deletingLastPathComponent(), withIntermediateDirectories: true)
-            try snapshotData.write(to: snapshotURL)
-        } catch {
-            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+            return URL(fileURLWithPath: String(describing: file))
+                .deletingLastPathComponent()
+                .appendingPathComponent("snapshots")
+                .appendingPathComponent("\(name).png")
         }
         
-    }
- 
+        private func makeSnapshotData(for snapshot: UIImage, file: StaticString, line: UInt) -> Data? {
+            guard let data = snapshot.pngData() else {
+                XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+                return nil
+            }
+            
+            return data
+        }
+    
+    
+    private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
+            let snapshotURL = makeSnapshotURL(named: name, file: file)
+            let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
+
+            do {
+                try FileManager.default.createDirectory(
+                    at: snapshotURL.deletingLastPathComponent(),
+                    withIntermediateDirectories: true
+                )
+                
+                try snapshotData?.write(to: snapshotURL)
+            } catch {
+                XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+            }
+        }
+     
 }
 
 extension UIViewController {
@@ -143,7 +181,7 @@ private class ImageStub: FeedImageCellControllerDelegate  {
     weak var controller: FeedImageCellController?
     
     init(description: String?, location: String?, image: UIImage?) {
-         viewModel = FeedImageViewModel(
+        viewModel = FeedImageViewModel(
             description: description,
             location: location,
             image: image,
@@ -156,7 +194,7 @@ private class ImageStub: FeedImageCellControllerDelegate  {
     }
     
     func didCancelImageRequest() {
-         
+        
     }
     
     
