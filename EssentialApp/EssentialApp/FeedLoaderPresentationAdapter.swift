f983cdcd27a7c3ed2d86c39c49fe09ce3e6c7b0d
loloa <lilia707@gmail.com>
2023-10-16 15:04:46 +0300

    combine

--- EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -5,33 +5,46 @@
 //  Created by אליסה לשין on 18/09/2023.
 //
 
+import Combine
 import EssentialFeed
 import EssentialFeediOS
 
- final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
     
-    private let feedLoader: FeedLoader
+    private var cancellable: Cancellable?
+    private let feedLoader: () -> FeedLoader.Publisher
     var presenter: FeedPresenter?
     
-    init(feedLoader: FeedLoader) {
+    init(feedLoader: @escaping () -> FeedLoader.Publisher) {
         self.feedLoader = feedLoader
     }
     
     func didRequestFeedrefresh() {
-        loadFeed()
-    }
-    
-    func loadFeed() {
-        presenter?.didStartLoadingFeed()
         
-        feedLoader.load { [weak self] result in
-            switch result {
-            case let .success(feed):
-                self?.presenter?.didFinishLoadingFeed(with: feed)
+        presenter?.didStartLoadingFeed()
+ 
+        cancellable = feedLoader().sink { [weak self] completion in
+            
+            switch completion {
+                
+            case .finished: break
             case let .failure(error):
                 self?.presenter?.didFinishLoadingFeed(with: error)
             }
+            
+        } receiveValue: { [weak self] feed in
+            self?.presenter?.didFinishLoadingFeed(with: feed)
         }
+        
+//
+//        feedLoader.load { [weak self] result in
+//            switch result {
+//            case let .success(feed):
+//                self?.presenter?.didFinishLoadingFeed(with: feed)
+//            case let .failure(error):
+//                self?.presenter?.didFinishLoadingFeed(with: error)
+//            }
+//        }
     }
 }
 
