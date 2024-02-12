bbe8d5a496c1b0a8f285f79ac25583a687add5c6
loloa <lilia707@gmail.com>
2024-02-12 13:03:25 +0200

    Reuse existing cellcontroller to avoid reallocaton ofresources

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -8,26 +8,35 @@ import UIKit
 import EssentialFeed
 import EssentialFeediOS
 
- final class FeedViewAdapter: ResourceView {
- 
+final class FeedViewAdapter: ResourceView {
+    
     private weak var controller: ListViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
     private let selection: (FeedImage) -> Void
-     
-     private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>
-     private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
-     
-     init(controller: ListViewController,
-          imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
-          selection: @escaping (FeedImage) -> Void) {
+    private let currentFeed: [FeedImage: CellControler]
+    
+    private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>
+    private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
+    
+    init(currentFeed:[FeedImage: CellControler] = [:], controller: ListViewController,
+         imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
+         selection: @escaping (FeedImage) -> Void) {
         self.controller = controller
+        self.currentFeed = currentFeed
         self.imageLoader = imageLoader
-         self.selection = selection
+        self.selection = selection
     }
-     
+    
     func display(_ viewModel: Paginated<FeedImage>) {
         
+        guard let controller = controller else {return}
+        var currentFeed = self.currentFeed
+        
         let feed: [CellControler] = viewModel.items.map { model in
+            
+            if let controller = currentFeed[model] {
+                return controller
+            }
             
             let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
                 
@@ -53,30 +62,36 @@ import EssentialFeediOS
                     return image
                 })
             
-            return CellControler(id: model, view)
+            let controller = CellControler(id: model, view)
+            currentFeed[model] = controller
+            return controller
         }
         
         guard let loadMorePublisher = viewModel.loadMorePublisher else {
-            controller?.display( feed )
+            controller.display( feed )
             return
         }
         let loadMoreAdapter = LoadMorePresentationAdapter(loader: loadMorePublisher)
         let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
-            
+        
         
         loadMoreAdapter.presenter = LoadResourcePresenter(
-            resourceView: self,
+            resourceView:  FeedViewAdapter(
+                currentFeed: currentFeed,
+                controller: controller,
+                imageLoader: imageLoader,
+                selection: selection),
             loadingView: WeakRefVirtualProxy(loadMore),
             errorView: WeakRefVirtualProxy(loadMore),
             mapper: { $0 })
-            
+        
         
         
         let loadMoreSection = [CellControler(id: UUID(), loadMore)]
         
-        controller?.display( feed, loadMoreSection)
+        controller.display( feed, loadMoreSection)
     }
 }
 private struct InvalidImageData: Error{}
-    
- 
+
+
